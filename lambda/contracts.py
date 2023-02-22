5cf3f7d1cde86187cfc560ad4093a6aed36c30ae
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-02-22 23:12:22 +0200

    refactor

--- lambda/contracts.py
+++ lambda/contracts.py
@@ -1,83 +1,87 @@
 import json
-import pkgutil
-import boto3
-import os
 from decimal import Decimal
-from utils.db_functions import (get_contracts,put_contract,get_contract,
-delete_contract,patch_contract,put_shifts,get_shifts,delete_shifts)
-from utils.utils import (serialize_float,serialize_int,epoch_to_date,
-check_response)
+from utils.db_functions import (get_contracts, put_contract, get_contract, 
+    delete_contract, patch_contract, put_shifts, get_shifts, delete_shifts)
+from utils.utils import (serialize_float, serialize_int, epoch_to_date, check_response)
+
 
 def handler(event, context):
     if event["pathParameters"] != None and "contract_id" in event["pathParameters"]:
         contract_id = event["pathParameters"]["contract_id"]
-    resource,method = event["resource"],event["httpMethod"]
-    dynamodb = boto3.resource('dynamodb')
-    contracts_table = dynamodb.Table(os.environ['CONTRACT_TABLE'])
-    shifts_table = dynamodb.Table(os.environ['SHIFTS_TABLE'])
-    return_object = {"statusCode":200,"headers":{"Content-Type":
-    "application/json"},"body":{}} 
+    resource, method = event["resource"], event["httpMethod"]
+    return_object = {"statusCode": 200, "headers": {"Content-Type":
+                                                    "application/json"}, "body": {}}
 
     try:
         if resource == "/contracts" and method == "GET":
             contracts = get_contracts()
             return_object["body"] = json.dumps({"contracts":
-            contracts},default=serialize_float)
-        
+                                                contracts}, default=serialize_float)
+
         elif resource == "/contracts" and method == "POST":
-            contract = json.loads(event['body'],parse_float=Decimal)
+            contract = json.loads(event['body'], parse_float=Decimal)
             contract["contract_id"] = context.aws_request_id
             response = put_contract(contract)
-            check_response(response,"malformed data")
-            return_object["body"] = json.dumps({"message":
-            "successfully created contract","contract": contract},
-            default=serialize_float)
-                                   
+            check_response(response, "malformed data")
+            return_object["body"] = json.dumps(
+                {"message": "successfully created contract", "contract": contract}, default=serialize_float)
+
         elif resource == "/contracts/{contract_id}" and method == "GET":
             contract = get_contract(contract_id)
-            return_object["body"] = json.dumps({"contract":contract},default=serialize_float)
-        
+            return_object["body"] = json.dumps(
+                {"contract": contract}, default=serialize_float)
+
         elif resource == "/contracts/{contract_id}" and method == "DELETE":
             response = delete_contract(contract_id)
-            check_response(response,"could not delete")
+            check_response(response, "could not delete")
+            shifts = get_shifts(contract_id, 0)
+            if len(shifts) > 0:
+                response = delete_shifts(contract_id, shifts)
+                check_response(response, "could not delete shifts")
             message = "successfully deleted contract %s" % (contract_id)
-            return_object["body"] = json.dumps({"message":message})            
-           
+            return_object["body"] = json.dumps({"message": message})
+
         elif resource == "/contracts/{contract_id}" and method == "PATCH":
-            contract = json.loads(event['body'],parse_float=Decimal)
+            contract = json.loads(event['body'], parse_float=Decimal)
             contract["contract_id"] = contract_id
             response = patch_contract(contract)
-            check_response(response,"could not update")
+            check_response(response, "could not update")
             message = "successfully updated contract %s" % (contract_id)
-            return_object["body"] = json.dumps({"message":message,"contract":response["Attributes"],
-            },default=serialize_float)
-        
+            return_object["body"] = json.dumps(
+                {"message": message, "contract": response["Attributes"]}, default=serialize_float)
+
         elif resource == "/contracts/{contract_id}/shifts" and method == "POST":
             shifts = json.loads(event['body'])["shifts"]
-            response = put_shifts(contract_id,shifts)
-            check_response(response,"could not create shifts")
-            message = "successfully created %s shifts under contract %s" % (len(shifts),contract_id)
-            return_object["body"] = json.dumps({"message":message})
+            response = put_shifts(contract_id, shifts)
+            check_response(response, "could not create shifts")
+            message = "successfully created %s shifts under contract %s" % (
+                len(shifts), contract_id)
+            return_object["body"] = json.dumps({"message": message})
 
         elif resource == "/contracts/{contract_id}/shifts/{start_time}/{end_time}" and method == "DELETE":
-            start_time,end_time = int(event["pathParameters"]["start_time"]),int(event["pathParameters"]["end_time"])
-            shifts = get_shifts(contract_id,start_time,end_time)
-            response = delete_shifts(contract_id,shifts)
-            check_response(response,"could not delete shifts")
-            message = "successfully deleted %s shifts starting %s and ending %s under contract %s" % (len(shifts),epoch_to_date(start_time),epoch_to_date(end_time),contract_id)
-            return_object["body"] = json.dumps({"message":message})
-        
+            start_time, end_time = int(event["pathParameters"]["start_time"]), int(
+                event["pathParameters"]["end_time"])
+            shifts = get_shifts(contract_id, start_time, end_time)
+            response = delete_shifts(contract_id, shifts)
+            check_response(response, "could not delete shifts")
+            message = "successfully deleted %s shifts starting %s and ending %s under contract %s" % (
+                len(shifts), epoch_to_date(start_time), epoch_to_date(end_time), contract_id)
+            return_object["body"] = json.dumps({"message": message})
+
         elif resource == "/contracts/{contract_id}/shifts/{start_time}/{end_time}" and method == "GET":
-            start_time,end_time = int(event["pathParameters"]["start_time"]),int(event["pathParameters"]["end_time"])
-            shifts = get_shifts(contract_id,start_time,end_time)
-            return_object["body"] = json.dumps({"shifts":shifts},default=serialize_int)
-       
+            start_time, end_time = int(event["pathParameters"]["start_time"]), int(
+                event["pathParameters"]["end_time"])
+            shifts = get_shifts(contract_id, start_time, end_time)
+            return_object["body"] = json.dumps(
+                {"shifts": shifts}, default=serialize_int)
+
         else:
             return_object["statusCode"] = 404
-            return_object["body"] = json.dumps({"message":"no matching resource"})
-    
+            return_object["body"] = json.dumps(
+                {"message": "no matching resource"})
+
     except Exception as error:
         return_object["statusCode"] = 400
-        return_object["body"] = json.dumps({"message":str(error)})
-    
+        return_object["body"] = json.dumps({"message": str(error)})
+
     return return_object
--- lambda/utils/db_functions.py
+++ lambda/utils/db_functions.py
@@ -5,30 +5,36 @@ dynamodb = boto3.resource('dynamodb')
 contracts_table = dynamodb.Table(os.environ['CONTRACT_TABLE'])
 shifts_table = dynamodb.Table(os.environ['SHIFTS_TABLE'])
 
+
 def get_contracts():
     contracts = contracts_table.scan()
     return contracts["Items"]
+
 
 def put_contract(contract):
     response = contracts_table.put_item(Item=contract)
     return response
 
+
 def get_contract(contract_id):
     contract = contracts_table.get_item(Key={'contract_id': contract_id})
     return contract["Item"]
+
 
 def delete_contract(contract_id):
     response = contracts_table.delete_item(Key={'contract_id': contract_id})
     return response
 
+
 def patch_contract(contract):
-    response = contracts_table.update_item(Key={"contract_id":contract["contract_id"]},
-    UpdateExpression="set base_pay=:base_pay, title=:title",
-    ExpressionAttributeValues={":base_pay":contract["base_pay"],":title":contract["title"]},
-    ReturnValues="ALL_NEW")
+    response = contracts_table.update_item(Key={"contract_id": contract["contract_id"]},
+                                           UpdateExpression="set base_pay=:base_pay, title=:title",
+                                           ExpressionAttributeValues={":base_pay": contract["base_pay"],
+                                           ":title": contract["title"]}, ReturnValues="ALL_NEW")
     return response
 
-def put_shifts(contract_id,shifts):
+
+def put_shifts(contract_id, shifts):
     try:
         with shifts_table.batch_writer() as batch:
             for shift in shifts:
@@ -37,25 +43,31 @@ def put_shifts(contract_id,shifts):
         response_code = 200
     except:
         response_code = 400
-    response = {"ResponseMetadata":{"HTTPStatusCode":response_code}}       
+    response = {"ResponseMetadata": {"HTTPStatusCode": response_code}}
     return response
 
-def get_shifts(contract_id,start_time,end_time):
-    shifts = shifts_table.query(
-    KeyConditionExpression="contract_id=:contract_id AND start_time>=:start_time",
-    FilterExpression="end_time<=:end_time",
-    ProjectionExpression="start_time,end_time",
-    ExpressionAttributeValues={":contract_id":contract_id,":start_time":start_time,":end_time":end_time}
-    )
+
+def get_shifts(contract_id, start_time, end_time=None):
+    args = dict(KeyConditionExpression="contract_id=:contract_id AND start_time>=:start_time",
+                ProjectionExpression="start_time,end_time",
+                ExpressionAttributeValues={":contract_id": contract_id, ":start_time": start_time})
+
+    if end_time != None:
+        args["FilterExpression"] = "end_time<=:end_time"
+        args["ExpressionAttributeValues"][":end_time"] = end_time
+
+    shifts = shifts_table.query(**args)
     return shifts["Items"]
 
-def delete_shifts(contract_id,shifts):
+
+def delete_shifts(contract_id, shifts):
     try:
         with shifts_table.batch_writer() as batch:
             for shift in shifts:
-                shifts_table.delete_item(Key={"contract_id":contract_id,"start_time": int(shift["start_time"])})
+                shifts_table.delete_item(
+                    Key={"contract_id": contract_id, "start_time": int(shift["start_time"])})
         response_code = 200
     except:
         response_code = 400
-    response = {"ResponseMetadata":{"HTTPStatusCode":response_code}}       
+    response = {"ResponseMetadata": {"HTTPStatusCode": response_code}}
     return response
