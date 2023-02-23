ed1af94342bc01d691724ad856f891f30afed76b
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-02-23 22:33:41 +0200

    fixing up

--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -2,28 +2,48 @@ from aws_cdk import (
     aws_dynamodb as dynamodb,
     aws_lambda as lambda_fn,
     aws_apigateway as apigw,
-    Stack,
+    Stack
 )
+import aws_cdk as core
 
 from constructs import Construct
+
 
 class CdkPythonStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        contract_table = dynamodb.Table(self, "ContractsTable",
-            partition_key=dynamodb.Attribute(name="contract_id", type=dynamodb.AttributeType.STRING))
+        contract_table = dynamodb.Table(
+            self, "ContractsTable",
+            partition_key=dynamodb.Attribute(
+                name="contract_id",
+                type=dynamodb.AttributeType.STRING))
 
-        shifts_table = dynamodb.Table(self, "ShiftsTable",
-            partition_key=dynamodb.Attribute(name="contract_id", type=dynamodb.AttributeType.STRING),
-            sort_key=dynamodb.Attribute(name="start_time", type=dynamodb.AttributeType.NUMBER))
+        shifts_table = dynamodb.Table(
+            self, "ShiftsTable",
+            partition_key=dynamodb.Attribute(
+                name="contract_id", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(
+                name="start_time",
+                type=dynamodb.AttributeType.NUMBER))
 
-        contracts_fn = lambda_fn.Function(self, "ContractsFunction",
-            runtime=lambda_fn.Runtime.PYTHON_3_9,
-            code=lambda_fn.Code.from_asset("lambda"),
+        lambda_layer = lambda_fn.LayerVersion(
+            self, 'LambdaLayer',
+            code=lambda_fn.AssetCode('layer/'),
+            compatible_runtimes=[lambda_fn.Runtime.PYTHON_3_8],
+        )
+
+        contracts_fn = lambda_fn.Function(
+            self, "ContractsFunction",
+            layers=[lambda_layer],
+            runtime=lambda_fn.Runtime.PYTHON_3_8,
+            code=lambda_fn.Code.from_asset(
+                "lambda"
+            ),
             handler="contracts.handler",
-            environment={'CONTRACT_TABLE': contract_table.table_name,"SHIFTS_TABLE":shifts_table.table_name})
+            environment={'CONTRACT_TABLE': contract_table.table_name,
+                         "SHIFTS_TABLE": shifts_table.table_name})
 
         contract_table.grant_read_write_data(contracts_fn)
         shifts_table.grant_read_write_data(contracts_fn)
@@ -32,7 +52,7 @@ class CdkPythonStack(Stack):
             self, 'ContractsEndpoint',
             handler=contracts_fn,
             proxy=False,
-            default_method_options={"api_key_required":True}
+            default_method_options={"api_key_required": True}
         )
 
         contracts = contracts_endpoint.root.add_resource("contracts")
@@ -46,23 +66,28 @@ class CdkPythonStack(Stack):
 
         shifts = contract.add_resource("shifts")
         shifts.add_method("POST")
-        
+
         start = shifts.add_resource("{start_time}")
         end = start.add_resource("{end_time}")
         end.add_method("DELETE")
         end.add_method("GET")
 
-        plan = contracts_endpoint.add_usage_plan("UsagePlan",
-            name="ContractApiPlan",
+        aggregate = contracts_endpoint.root.add_resource("aggregate")
+        user_aggregate = aggregate.add_resource("{user_name}")
+        user_aggregate.add_method("GET")
+
+        contract_aggregate = user_aggregate.add_resource("{contract_id}")
+        contract_aggregate.add_method("GET")
+
+        plan = contracts_endpoint.add_usage_plan(
+            "UsagePlan", name="ContractApiPlan",
             throttle=apigw.ThrottleSettings(
                 rate_limit=10,
                 burst_limit=2
             ),
-            api_stages=[apigw.UsagePlanPerApiStage(stage=contracts_endpoint.deployment_stage)]
+            api_stages=[apigw.UsagePlanPerApiStage(
+                stage=contracts_endpoint.deployment_stage)]
         )
 
         key = contracts_endpoint.add_api_key("ApiKey")
         plan.add_api_key(key)
-   
-
-       
